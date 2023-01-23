aaa65ce60948b0dbc97ade32fa3c8e396958cf92
Valto Silva <valtosilva7@gmail.com>
2023-01-23 11:40:15 -0300

    Completed Sign in functionality

--- README.md
+++ README.md
@@ -81,6 +81,8 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/t
 
 05-Complete_OAuth_functionality
 
+06-Complete_Sign-in_functionality
+
 1:32:11 Create Sign in, Sign up and Forgot password pages and OAuth component
 2:58:11 Install Firebase and react-toastify and sign up the user
 4:10:01 Complete the OAuth functionality
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,18 +1,41 @@
 import { useState } from "react";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import { toast } from "react-toastify";
+
 import OAuth from "../components/OAuth";
+import { async } from "@firebase/util";
 
 export default function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({ email: "", password: "" });
   const { email, password } = formData;
+  const navigate = useNavigate();
 
   function onChange(e) {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value,
     }));
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      if (userCredential.user) {
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error("Bad user credentials!");
+    }
   }
 
   return (
@@ -27,7 +50,7 @@ export default function SignIn() {
           />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-6"
               type="email"
