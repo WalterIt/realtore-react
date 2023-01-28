25b46de58737da0fb3508ae1ebd99f453a2ea8c4
Valto Silva <valtosilva7@gmail.com>
2023-01-28 12:21:52 -0300

    Created Slider Component and added it to the Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,10 @@
+import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
+import Slider from "../components/Slider";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Slider />
+    </div>
+  );
 }
